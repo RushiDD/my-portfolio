89e36958a6b44372ee71309c4782f08e2dbc3f66
RushiDD <rushidhamanage.7@gmail.com>
2025-05-29 12:04:24 +0530

    Updated name and picture

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import Contact from "./components/Contact";
 
 export default function App() {
   return (
-    <div className="overflow-x-hidden text-stone-300 antialised">
+    <div className="text-stone-300 antialised">
       <div className="fixed inset-0 -z-10">
         <div className="relative h-full w-full bg-black">
           <div className="absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]"></div>
@@ -15,8 +15,8 @@ export default function App() {
         </div>
       </div>
 
-      <div className="w-full overflow-hidden">
-        <div className="inline-block scale-[0.9] origin-top container px-8">
+      <div className="w-full h-screen flex justify-center items-start">
+        <div className="scale-[0.9] origin-top w-full max-w-screen-xl px-8">
           <Navbar />
           <Hero />
           <Technologies />
